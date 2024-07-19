85abb1267ad17d4d70d4480ab266464c7a2709e3
Luis Meza <ednilsonluis.11@gmail.com>
2024-07-18 23:54:35 -0500

    Fin diseño pagina inicio

---
+++ src/pages/Capacitaciones.tsx
@@ -0,0 +1,10 @@
+
+const Capacitaciones = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Capacitaciones
--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,11 +1,26 @@
-import NabBar from "../components/NabBar"
+import NabBar from "../components/NabBar";
+import '../stylesheets/Inicio.css'; 
 
 const Inicio = () => {
   return (
     <>
-    <NabBar/> 
+      <NabBar /> 
+      <main className="inicio">
+        <h1 className="inicio__title">Bienvenido a Certus!!</h1>
+        <div className="inicio__modal modal">
+          <div className="modal__info">
+            <h2 className="modal__info-title">Mostrar capacitaciones pendientes?</h2>
+            <div className="modal__buttons">
+              <button className="modal__button">Sí</button>
+              <button className="modal__button">No</button>
+            </div>
+            <p className="modal__info-text">Capacitaciones pendientes: 5</p>
+          </div>
+          <div className="modal__image image_modal"></div>
+        </div>
+      </main>
     </>
   )
 }
 
-export default Inicio
+export default Inicio;
